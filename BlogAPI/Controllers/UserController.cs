61ed6672d730db20b74f74ee8adffa39d2953c93
bishwash369 <bishwashparajuli10@gmail.com>
2020-12-04 04:45:24 +0545

    Models added and Dtos created

--- BlogAPI/Controllers/UserController.cs
+++ BlogAPI/Controllers/UserController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BlogAPI.Models;
+using BlogAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +10,34 @@ using System.Threading.Tasks;
 
 namespace BlogAPI.Controllers
 {
-    [Route("api/[controller]")]
+
     [ApiController]
     public class UserController : ControllerBase
     {
+        public readonly BlogDbContext db;
+        public UserController(BlogDbContext DbContext)
+        {
+            db = DbContext;
+        }
+
+        [HttpGet]
+        [Route("api/GetAllUsers")]
+        public async Task<List<UserDto>> GetAllUsers()
+        {
+            return await db.Users.Select(x => new UserDto
+            {
+                Name = x.Name,
+                Address = x.Address,
+                Email = x.Email,
+                
+            }).ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("api/GetUser/{id}")]
+        public async Task<UserDto> GetUser(int id)
+        {
+            return await db.Users.Where(x => x.Id == id).FirstOrDefault();
+        }
     }
 }
