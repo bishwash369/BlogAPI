d07951d85fa0b2409ce718d4a7f45341f80c76d8
bishwash369 <bishwashparajuli10@gmail.com>
2020-12-04 04:33:17 +0545

    IntialCommit

---
+++ BlogAPI/Models/BlogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogAPI.Models
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {  }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ BlogAPI/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogAPI.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int AuthorId { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public virtual User Users { get; set; }
+    }
+}
